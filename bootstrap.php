25365ee943d3a0598ced68ec80b43fec07e7660e
Ruy Fialho <ruyfialho@ruyfialho.com>
2017-04-17 15:00:59 -0300

    initial commit

---
+++ bootstrap.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Collapsible Content plugin
+ *
+ * @package         KnowTheCode\CollapsibleContent
+ * @author          hellofromTonya
+ * @license         GPL-2.0+
+ * @link            https://KnowTheCode.io
+ *
+ * @wordpress-plugin
+ * Plugin Name:     Collapsible Content
+ * Plugin URI:      _
+ * Description:     Collapsible Content is a WordPress Plugin that shows and hides hidden content.  Practical examples include Q&As, FAQs, hints, marketing teasers, and more.  Click the icon to open and reveal the content. Click again to close and hide it.
+ * Version:         1.0.0
+ * Author:          hellofromTonya
+ * Author URI:      https://KnowTheCode.io
+ * Text Domain:     collapsible_content
+ * Requires WP:     4.7
+ * Requires PHP:    5.5
+ */
+
+/*
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+namespace KnowTheCode\CollapsibleContent;
---
+++ src/shortcode/shortcodes.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Description
+ *
+ * @package     ${NAMESPACE}
+ * @since       1.0.0
+ * @author      ruyfialho
+ * @link        http://ruyfialho.com
+ * @license     GNU General Public License 2.0+
+ */
